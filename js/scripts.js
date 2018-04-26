74a63b0948c73aa0b7ddbeb004860ced3e5ccef4
Anthony-Nderitu <anderitu1997@yahoo.com>
2018-04-26 16:07:25 +0300

    Add more on calculator.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,45 @@
 var add = function(number1, number2) {
 return number1 + number2;
 };
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
 
-alert(add(number1,number2));
+var subtract = function (number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function (number1, number2) {
+  return number1 * number2;
+};
+var divide = function (number1, number2) {
+  return number1 / number2;
+};
+
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+  event.preventDefault();
+  var number1 = parseInt($("#add1").val());
+  var number2 = parseInt($("#add2").val());
+  var result = add(number1, number2);
+  $("#outputadd").text(result);
+  });
+});
+
+$(document).ready(function() {
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#subtract1").val());
+    var number2 = parseInt($("#subtract2").val());
+    var result = subtract(number1, number2);
+    $("#outputsubtract").text(result);
+  });
+});
+
+$(document).ready(function() {
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#multiply1").val());
+    var number2 = parseInt($("#multiply2").val());
+    var result = multiply(number1, number2);
+    $("#outputmultiply").text(result);
+  })
+})
